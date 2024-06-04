8d20da32b722aa6a9e63c17262944c295acc8cbc
Halisson da Silva Jesus <halissonsj@icloud.com>
2024-06-04 16:41:00 -0300

    Componentes da home criados

--- CryptoTracker/Core/Home/Views/CoinRowView.swift
+++ CryptoTracker/Core/Home/Views/CoinRowView.swift
@@ -8,11 +8,93 @@
 import SwiftUI
 
 struct CoinRowView: View {
+    
+    let coin: CoinModel
+    let showHoldingsColumn: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0) {
+            leftColumn
+            
+            Spacer()
+            
+            if showHoldingsColumn {
+                centerColumn
+            }
+            
+            rightColumn
+        }
+        .font(.subheadline)
     }
 }
 
-#Preview {
-    CoinRowView()
+#Preview(traits: .sizeThatFitsLayout) {
+    CoinRowView(coin: CoinModel(id: "bitcoin",
+                                symbol: "btc",
+                                name: "Bitcoin",
+                                image: "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
+                                currentPrice: 70670,
+                                marketCapRank: 1,
+                                marketCap: 1390232065477,
+                                fullyDilutedValuation: 1481395448345, 
+                                totalVolume: 24747719623,
+                                high24H: 70973, 
+                                low24H: 68544,
+                                priceChange24H: 1478.96,
+                                priceChangePercentage24H: 2.13749,
+                                marketCapChange24H: 24325434656,
+                                marketCapChangePercentage24H: 1.7809,
+                                circulatingSupply: 19707684,
+                                totalSupply: 21000000,
+                                maxSupply: 21000000,
+                                ath: 73738,
+                                athChangePercentage: -4.33331,
+                                athDate: "2024-03-14T07:10:36.635Z",
+                                atl: 67.81,
+                                atlChangePercentage: 103931.38285,
+                                atlDate: "2013-07-06T00:00:00.000Z",
+                                lastUpdated: "2024-06-04T18:12:19.211Z"),
+                showHoldingsColumn: true)
+}
+
+extension CoinRowView {
+    
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundColor(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            
+            Circle()
+                .frame(width: 30, height: 30)
+            
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundColor(Color.theme.accent)
+        }
+    }
+    
+    private var centerColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                .bold()
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
+    }
+    
+    private var rightColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundColor(Color.theme.accent)
+            
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundColor(
+                    (coin.priceChangePercentage24H ?? 0) >= 0 ? Color.theme.green : Color.theme.red
+                )
+        }
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+    }
 }
--- CryptoTracker/Core/Home/Views/HomeView.swift
+++ CryptoTracker/Core/Home/Views/HomeView.swift
@@ -8,11 +8,55 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showPortfolio = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            //background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // content layer
+            VStack {
+                HStack {
+                    homeHeader
+                }
+                Spacer(minLength: 0)
+            }
+        }
     }
 }
 
 #Preview {
-    HomeView()
+    NavigationView {
+        HomeView()
+            .navigationBarHidden(true)
+    }
+}
+
+extension HomeView {
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none, value: showPortfolio)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio))
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
 }
