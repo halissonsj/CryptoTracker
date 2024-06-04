ffb70402ad73714d3d476b7fd05d544ac7e7f87f
Halisson da Silva Jesus <halissonsj@icloud.com>
2024-06-04 11:15:30 -0300

    Commit Inicial. Setup de cores e imagens do projeto

---
+++ CryptoTracker/Extensions/Color.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  CryptoTracker
+//
+//  Created by Halisson da Silva Jesus on 04/06/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    
+    let accent = Color("accentColor")
+    let background = Color("backgroundColor")
+    let green = Color("greenColor")
+    let red = Color("redColor")
+    let secondaryText = Color("secondaryTextColor")
+    
+}
